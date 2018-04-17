7fcc21b440ddeb39949a3364efd77bcf231e6352
Sean C Davis <scdavis41@gmail.com>
2018-04-17 18:35:45 -0400

    Simplify configuration options

--- lib/heartwood/uploader/configuration.rb
+++ lib/heartwood/uploader/configuration.rb
@@ -13,35 +13,8 @@ module Heartwood
     end
 
     class Configuration
-      attr_accessor :acl, # Access control list https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl (default: 'public-read')
-                    :aws_access_key_id,
-                    :aws_bucket,
-                    :aws_secret_access_key,
-                    :expiration, # Time after upload to clean up incomplete multipart uploads
-                    :form_id, # HTML #id selector for form element (default: 'fileupload')
-                    :max_file_size, # Maximum allowed file size (default: 50MB)
-                    :multipart, # Allow multiple file uploads (default: false)
-                    :prefix # Prefix (path) to store the file (default: nil)
 
-      def acl
-        @acl ||= 'private'
-      end
-
-      def expiration
-        @expiration ||= 10.hours.from_now
-      end
-
-      def form_id
-        @form_id ||= 'heartwood-uploader'
-      end
-
-      def max_file_size
-        @max_file_size ||= 50.megabytes
-      end
-
-      def multipart
-        @multipart ||= false
-      end
+      attr_accessor :aws_access_key_id, :aws_bucket, :aws_secret_access_key
 
     end
   end
